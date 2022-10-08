57b63202f9e5d0282d32982971f2c3087afdad0f
Giancarlos Gutiérrez Cervantes <ggutierrez@canvia.com>
2022-10-08 17:06:38 -0500

    JWT Authentication

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /*
  * Esta clase se genera para poder hacer uso de las credenciales generadas en el service PlatziUserDetailsService
@@ -15,8 +23,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PlatziUserDetailsService platziUserDetailsService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(platziUserDetailsService);
+    }
+
+    // metodo para permitir hacer peticiones sin estar autenticado, en este caso para 'authenticate', pero solicitando autenticacion para las demas
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // agregado para que el token maneje la sesion
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class); // se agrega el filtro
+    }
+
+    // ya que se usa el authentication manager en AuthController
+    @Override
+    @Bean   // agregado para decir explicitamente que se este metodo el gestor de la autenticacion
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+                "/swagger-resources/**", "/configuration/security",
+                "/swagger-ui/index.html", "/webjars/**");
     }
 }
---
+++ src/main/java/com/platzi/market/web/security/filter/JwtFilterRequest.java
@@ -0,0 +1,50 @@
+package com.platzi.market.web.security.filter;
+
+import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilterRequest extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private PlatziUserDetailsService platziUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+            String jwt = authorizationHeader.substring(7);
+            String username = jwtUtil.extractUsername(jwt);
+
+            // verificar que no exista una autenticacion para un usuario
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = platziUserDetailsService.loadUserByUsername(username);    // verificar el usuario
+
+                if (jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()); //crear sesion para el user
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // poner los detalles de la conexion, (navegador, hora de conexion, SO, etc)
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);    // para que no sea necesario validar nuevamente si hace otra peticion el mismo user
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
